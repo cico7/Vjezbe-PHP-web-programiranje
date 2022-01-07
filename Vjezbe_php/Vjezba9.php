3558a8b5ea4ebc61c459b1a871bdac25e6f4a728
Damir Eri <72521965+cico7@users.noreply.github.com>
2022-01-07 13:43:08 +0100

    Add files via upload

--- Vjezbe_php/Vjezba9.php
+++ Vjezbe_php/Vjezba9.php
@@ -9,63 +9,38 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 	</head>
-<body>
+    <body>
 <?php
 
-function daliradi()
-{
-    $currentDate = (new DateTime())->format('l H:i:s')
-    if(date('N', $currentDate)<=5 && date('H', $currentDate) >=8 && date('H', $currentDate) <=20)
+/*$date = '2016-11-16 05:35:14';
+set locale(LC_TIME, array('nb_NO.UTF-8','nb_NO@norw','nb_NO','croatian'));
+echostrftime("%e %b %Y",strtotime($date));
+*/
+$today = strtotime(date("l, H:i:s"));
+print "TRENUTNI DAN I VRIJEME: " . date("l, H:i:s");
+print "<br>";
+
+function radnoV($currentDate)
     {
-        
+        if (date('N', $currentDate) <= 5 && date('H', $currentDate) >= 8 && date('H', $currentDate) <= 20)
+        {
+            $result = "OTVORENO OD 08 DO 20.";
+        }
+        else if (date('N', $currentDate) == 6 && date('H', $currentDate) >= 8 && date('H', $currentDate) <= 15)
+        {
+            $result = "OTVORENO OD 08 DO 15.";
+        }
+        else if (date('N', $currentDate) == 7)
+        {
+            $result = "NEDJELJOM ZATVORENO.";
+        }
+        else
+        {
+            $result = "ZATVORENO";
+        }
+        return $result;
     }
-    else if (date('N', $currentDate)==6 && date('H', $currentDate) >=8 && date('H', $currentDate) <=15)
-    {
-
-    }
-    else if (date('N', $currentDate)==7
-    {
-
-    }
-    else
-    {
-        $result = "Ne radi";
-    }
-    return result;
-}
-
-
-
-$a = 8;
-$b = 20;
-$c = 15;
-$x = "Saturday";
-$y = "Sunday";
-
-function getDatetimeNow() {
-  $datetime = new DateTime();
-  return $datetime->format('l H:i:s');
-}
-$currentDate = getDatetimeNow();
-$currentDate = (new DateTime()) -> format ('l H:i:s');
-echo "<h1>Radno vrijeme</h1>
-<p>Radno vrijeme trgovine je svaki dan od 8:00 do 20:00</p>
-<p>Radno vrijeme trgovine subotom od 8:00 do 15:00</p>
-<p>Nedjeljom ne radimo</p>
-<p>Sada je $currentDate</p>";
-If($currentDate == $y){
-    echo "<p>ZATVORENO</p>";
-}
-elseif($currentDate == $x || $currentDate >= $a || $currentDate <= $c ){
-    echo "<p>OTVORENO</p>";
-}
-
-elseif ( $currentDate >= $a || $currentDate <= $b ){
-	echo "<p>OTVORENO</p>";
-}
-else{
-    echo "<p>ZATVORENO</p>";
-}
-	?>
+    print radnoV($today);
+    ?>
 </body>
 </html>
